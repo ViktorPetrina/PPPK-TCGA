7c39b55cd5e4bc579a2560bec5865eccb6891214
Viktor <viktor.petrina01@gmail.com>
2025-02-06 01:04:33 +0100

    scrapper refactor

--- TCGA_Scrapper/Program.cs
+++ TCGA_Scrapper/Program.cs
@@ -5,11 +5,8 @@ using TCGA_Scrapper.Utilities;
 
 namespace TCGA_Scrapper
 {
-    // TODO: refaktorirat
     internal class Program
     {
-        private const long WAIT_LENGTH = 10;
-
         private const string REQUIRED_TEXT = "IlluminaHiSeq pancan normalized";
         private const string DOWNLOAD_URL_SUBSTRING = "sampleMap%";
         private const string DOWNLOAD_URLS_PATH = "download_urls.txt";
@@ -23,77 +20,19 @@ namespace TCGA_Scrapper
         private static IList<string> firstLevelUrls = new List<string>();
         private static IList<string> secondLevelUrls = new List<string>();
         private static IList<string> downloadUrls = new List<string>();
-        private static IList<string> compressedFiles = new List<string>();
 
         static async Task Main(string[] args)
         {
             ScrapeWeb();
-            LoadUrlsFromFile();
-            await DownloadFiles();
-            LoadCompressedFiles();
-            UnpackFiles();
+
+            await DownloadAndUnpackFiles();
         }
 
-        private static void LoadCompressedFiles()
+        private static async Task DownloadAndUnpackFiles()
         {
-            Directory.GetFiles(COMPRESSED_FILES_PATH, "*.gz").ToList().ForEach(compressedFiles.Add);
-        }
-
-        private static void UnpackFiles()
-        {
-            Directory.CreateDirectory(DECOMPRESSED_FILES_PATH);
-
-            foreach (var file in compressedFiles)
-            {
-                try
-                {
-                    string fileName = Path.GetFileNameWithoutExtension(file);
-                    string destination = Path.Combine(DECOMPRESSED_FILES_PATH, fileName);
-                    FileUtils.DecompressFile(file, destination);
-                    Console.WriteLine($"File {file} extracted successfully to {destination}.");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"An error occurred while extracting {file}: {e.Message}");
-                }
-            }
-        }
-
-        private static void LoadUrlsFromFile()
-        {
-            File.ReadLines(DOWNLOAD_URLS_PATH).ToList().ForEach(downloadUrls.Add);
-        }
-
-        private static async Task DownloadFiles()
-        {
-            Directory.CreateDirectory(COMPRESSED_FILES_PATH);
-
-            using (HttpClient client = new HttpClient { Timeout = TimeSpan.FromMinutes(1) })
-            {
-                int counter = 1;
-                foreach (string url in downloadUrls)
-                {
-                    string fileName = Path.GetFileName(new Uri(url).LocalPath);
-
-                    string uniqueFileName = $"{Path.GetFileNameWithoutExtension(fileName)}_{counter}{Path.GetExtension(fileName)}";
-
-                    string filePath = Path.Combine(COMPRESSED_FILES_PATH, uniqueFileName);
-
-                    try
-                    {
-                        Console.WriteLine($"\nDownloading {uniqueFileName}...");
-                        byte[] fileBytes = await client.GetByteArrayAsync(url);
-                        await File.WriteAllBytesAsync(filePath, fileBytes);
-                        Console.WriteLine($"{uniqueFileName} downloaded.");
-                    }
-                    catch (TaskCanceledException e)
-                    {
-                        Console.WriteLine($"Download timeout. Exception: {e.Message}");
-                    }
-
-                    counter++;
-                }
-            }
+            downloadUrls = File.ReadLines(DOWNLOAD_URLS_PATH).ToList();
+            await FileUtils.DownloadFiles(COMPRESSED_FILES_PATH, downloadUrls);
+            FileUtils.DecompressFilesToDirectory(DECOMPRESSED_FILES_PATH, Directory.GetFiles(COMPRESSED_FILES_PATH, "*.gz"));
         }
 
         private static void ScrapeWeb()
@@ -123,66 +62,6 @@ namespace TCGA_Scrapper
 
             Console.WriteLine("Saving to file...");
             File.WriteAllLines(DOWNLOAD_URLS_PATH, downloadUrls);
-        }
-
-        private static void FillFirstLevelUrls(IWebDriver driver, WebDriverWait wait) 
-        {
-            driver.Navigate().GoToUrl(FIRST_LEVEL_URL);
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(WAIT_LENGTH));
-            wait.Until(d => d.FindElements(By.CssSelector(".Datapages-module__list___2yM9o li")).Count >= 6);
-
-            var listItems = driver.FindElements(By.CssSelector(".Datapages-module__list___2yM9o li"));
-            foreach (var item in listItems)
-            {
-                var aTag = item.FindElement(By.CssSelector("a"));
-                string href = aTag.GetAttribute("href");
-                firstLevelUrls.Add(href);
-                Console.WriteLine($"Added url: {href}\n");
-            }
-        }
-
-        private static void FillSecondLevelUrls(IWebDriver driver, WebDriverWait wait)
-        {
-            foreach (var url in firstLevelUrls)
-            {
-                driver.Navigate().GoToUrl(url);
-                wait = new WebDriverWait(driver, TimeSpan.FromSeconds(WAIT_LENGTH));
-                wait.Until(d => d.FindElement(By.CssSelector(".Datapages-module__groupList___1h_hi")));
-
-                try
-                {
-                    var aTag = driver.FindElement(By.LinkText(REQUIRED_TEXT));
-                    string href = aTag.GetAttribute("href");
-                    secondLevelUrls.Add(href);
-                    Console.WriteLine($"Added url: {href}\n");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("No pancan normalized, exception: " + ex.Message + '\n');
-                }
-            }
-        }
-
-        private static void FillThirdLevelUrls(IWebDriver driver, WebDriverWait wait)
-        {
-            foreach (var url in secondLevelUrls)
-            {
-                driver.Navigate().GoToUrl(url);
-                wait = new WebDriverWait(driver, TimeSpan.FromSeconds(WAIT_LENGTH));
-                wait.Until(d => d.FindElement(By.CssSelector(".Datapages-module__value___3k05o")));
-
-                try
-                {
-                    var aTag = driver.FindElement(By.XPath($"//a[contains(text(), '{DOWNLOAD_URL_SUBSTRING}')]"));
-                    string href = aTag.GetAttribute("href");
-                    downloadUrls.Add(href);
-                    Console.WriteLine($"Added url: {href}\n");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("No download url, exception: " + ex.Message + '\n');
-                }
-            }
         }
     }
 }
--- TCGA_Scrapper/Utilities/FileUtils.cs
+++ TCGA_Scrapper/Utilities/FileUtils.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO.Compression;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO.Compression;
 
 namespace TCGA_Scrapper.Utilities
 {
@@ -11,12 +6,64 @@ namespace TCGA_Scrapper.Utilities
     {
         public static void DecompressFile(string source, string destination)
         {
-                using (FileStream compressedStream = new FileStream(source, FileMode.Open, FileAccess.Read))
-                using (FileStream outputStream = new FileStream(destination, FileMode.Create, FileAccess.Write))
-                using (GZipStream decompressionStream = new GZipStream(compressedStream, CompressionMode.Decompress))
+            using (FileStream compressedStream = new FileStream(source, FileMode.Open, FileAccess.Read))
+            using (FileStream outputStream = new FileStream(destination, FileMode.Create, FileAccess.Write))
+            using (GZipStream decompressionStream = new GZipStream(compressedStream, CompressionMode.Decompress))
+            {
+                decompressionStream.CopyTo(outputStream);
+            }
+        }
+
+        public static async Task DownloadFiles(string dir, IEnumerable<string> urls)
+        {
+            Directory.CreateDirectory(dir);
+
+            using (HttpClient client = new HttpClient { Timeout = TimeSpan.FromMinutes(1) })
+            {
+                int counter = 1;
+                foreach (string url in urls)
                 {
-                    decompressionStream.CopyTo(outputStream);
+                    string fileName = Path.GetFileName(new Uri(url).LocalPath);
+
+                    string uniqueFileName = $"{Path.GetFileNameWithoutExtension(fileName)}_{counter}{Path.GetExtension(fileName)}";
+
+                    string filePath = Path.Combine(dir, uniqueFileName);
+
+                    try
+                    {
+                        Console.WriteLine($"\nDownloading {uniqueFileName}...");
+                        byte[] fileBytes = await client.GetByteArrayAsync(url);
+                        await File.WriteAllBytesAsync(filePath, fileBytes);
+                        Console.WriteLine($"{uniqueFileName} downloaded.");
+                    }
+                    catch (TaskCanceledException e)
+                    {
+                        Console.WriteLine($"Download timeout. Exception: {e.Message}");
+                    }
+
+                    counter++;
                 }
+            }
+        }
+
+        public static void DecompressFilesToDirectory(string path, IEnumerable<string> files)
+        {
+            Directory.CreateDirectory(path);
+
+            foreach (var file in files)
+            {
+                try
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(file);
+                    string destination = Path.Combine(path, fileName);
+                    DecompressFile(file, destination);
+                    Console.WriteLine($"File {file} extracted successfully to {destination}.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"An error occurred while extracting {file}: {e.Message}");
+                }
+            }
         }
     }
 }
